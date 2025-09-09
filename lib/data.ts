66841b7df5ca9298ec4305d5446d8d7eee381c7b
oskaro01 <oskarode80@gmail.com>
2025-09-09 16:25:33 +0600

    04-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -1,36 +1,25 @@
 const data = {
-  headerMenus: [
+  carousels: [
     {
-      name: "Today's Deal",
-      href: '/search?tag=todays-deal',
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
     },
     {
-      name: 'New Arrivals',
-      href: '/search?tag=new-arrival',
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
     },
     {
-      name: 'Featured Products',
-      href: '/search?tag=featured',
-    },
-    {
-      name: 'Best Sellers',
-      href: '/search?tag=best-seller',
-    },
-    {
-      name: 'Browsing History',
-      href: '/#browsing-history',
-    },
-    {
-      name: 'Customer Service',
-      href: '/page/customer-service',
-    },
-    {
-      name: 'About Us',
-      href: '/page/about-us',
-    },
-    {
-      name: 'Help',
-      href: '/page/help',
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
     },
   ],
 }
