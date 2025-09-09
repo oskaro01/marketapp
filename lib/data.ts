5e49d019e141c6dd48cbba8a43be3a178135653d
oskaro01 <oskarode80@gmail.com>
2025-09-09 17:16:29 +0600

    04-carousel-1

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,39 @@
 const data = {
+  headerMenus: [
+    {
+      name: "Today's Deal",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrival',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-seller',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+  ],
+
   carousels: [
     {
       title: 'Most Popular Shoes For Sale',
@@ -21,7 +56,8 @@ const data = {
       url: '/search?category=Wrist Watches',
       isPublished: true,
     },
-  ],
+  ]
+
 }
 
 export default data
