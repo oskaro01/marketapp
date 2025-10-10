6b888834df91c3d57faf8b8b3435f74b32b2706a
oskaro01 <oskarode80@gmail.com>
2025-10-11 01:51:07 +0600

    14-User-Signin

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,13 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
+
 export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An Amazon clone built with Next.js and MongoDB'
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
